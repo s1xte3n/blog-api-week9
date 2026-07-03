e73dd98e412598e5049649a8de12677a74245ca0
Ntlantla Thokozani Nhlapo <ntnhlapo16@gmail.com>
2026-07-03 02:42:00 +0200

    submit

--- database/connectDB.js
+++ database/connectDB.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
+
+        await Article.syncIndexes();
+
         console.log('MongoDB Connected Successfully');
     } catch (error) {
         console.error('DB Connection Failed', error.message);
